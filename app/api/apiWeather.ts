a2fa04284368f2a2820e8c64c6154ce907fcf5e6
EdiJunior88 <er.moura88@yahoo.com.br>
2024-01-15 22:08:27 -0300

    Update

--- app/api/apiWeather.ts
+++ app/api/apiWeather.ts
@@ -1,8 +1,8 @@
-const axios = requie('axios')
+import axios from 'axios'
 const apiWeather = async (value: string) => {
   try {
     const config = useRuntimeConfig()
-    const apiKey = config.public.NUXT_API_KEY
+    const apiKey = config.NUXT_API_KEY
     const response = await axios.get(
       `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${value}&days=1&aqi=yes`
     )
